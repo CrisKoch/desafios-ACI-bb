7a667e5c612b91e93bbbfa80c1246f190855bbe9
Cristina Kochmann <cristinak.iftm@gmail.com>
2022-10-30 18:01:54 -0300

    Primeira versão

---
+++ Desafio-3/script.js
@@ -0,0 +1,31 @@
+document.getElementById("btnSubmit").onclick = function () {
+  var radios = document.getElementsByName("perfil");
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      carregaInfo(radios[i].value);
+    }
+  }
+};
+
+function carregaInfo(id) {
+  switch (id) {
+    case "private":
+      abreModal("investimentos");
+      break;
+
+    case "exclusivo":
+      abreModal("financiamentos");
+      break;
+
+    case "carteirao":
+      abreModal("renegociacaoDividas");
+      break;
+
+    default:
+      break;
+  }
+}
+
+function abreModal(tipo) {
+  window.open(`${tipo}.html`, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
